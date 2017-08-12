19bb2206064d0e2426d4f93b532e3d4cd85b95fa
MKA1500 <maciejkabsch@gmail.com>
2017-08-12 14:57:53 +0200

    config updated

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
     entry: [
         'webpack-dev-server/client?http://127.0.0.1:8080/',
         'webpack/hot/only-dev-server',
+        'bootstrap-loader',
         './src'
     ],
     output: {
@@ -14,7 +15,7 @@ module.exports = {
     },
     resolve: {
         modulesDirectories: ['node_modules', 'src'],
-        extensions: ['', '.js']
+        extension: ['', '.js', '.scss']
     },
     module: {
         loaders: [
@@ -29,6 +30,23 @@ module.exports = {
             {
                 test: /\.html$/,
                 loader: 'raw'
+            },
+            {
+                test: /\.scss$/,
+                loaders: [
+                    'style',
+                    'css',
+                    'autoprefixer?browsers=last 3 versions',
+                    'sass?outputStyle=expanded'
+                ]
+            },
+            {
+                test: /\.(woff2?|ttf|eot|svg)$/,
+                loader: 'url?limit=10000'
+            },
+            {
+                test: /bootstrap-sass\/assets\/javascripts\//,
+                loader: 'imports?jQuery=jquery'
             }
         ]
     },
@@ -42,5 +60,4 @@ module.exports = {
             '*': 'http://localhost:3000'
         }
     }
-}
-;
+};
