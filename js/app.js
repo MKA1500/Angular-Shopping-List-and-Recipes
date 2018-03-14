b6e1fe278cdc565b3be7d39af71e3c63d35e60be
MKA1500 <maciejkabsch@gmail.com>
2018-03-14 22:04:27 +0100

    adding new posts

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 var app = angular.module('app', []);
 app.controller('PostCtrl', function($scope){
+  $scope.addPost = function(){
+    if($scope.postBody) {
+      $scope.posts.unshift({
+        username: 'Macko666',
+        body: $scope.postBody
+      });
+      $scope.postBody = '';
+    }
+  }
   $scope.posts = [
     {
       username: 'Macko666',
