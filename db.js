cd37a740b1c7bc60567ca82af5e13c65f4e6ef9a
MKA1500 <maciejkabsch@gmail.com>
2018-07-29 20:13:54 +0200

    reissue

--- db.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/social', function(){
-  console.log('we have a connection with Mongo DB');
-});
-module.exports = mongoose;
