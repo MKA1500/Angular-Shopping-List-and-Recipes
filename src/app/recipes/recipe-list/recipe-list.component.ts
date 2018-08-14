cc4d6e56e4dfa77108e26edcda454efb475c31fa
MKA1500 <maciejkabsch@gmail.com>
2018-08-14 17:42:42 +0200

    hands on

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,6 +12,11 @@ export class RecipeListComponent implements OnInit {
       'Testing Recipe',
       'This is simply a test',
       'http://bi.im-g.pl/im/b9/ab/16/z23770809IF,Mojito-to-doskonaly-sposob-na-orzezwienie-w-cieple.jpg'
+    ),
+    new Recipe(
+      'Kanapka z jajkiem i awokado',
+      'This is simply a test',
+      'http://bi.im-g.pl/im/8f/ae/16/z23783567IF,Pelnowartosciowe-zrodla-bialka--to-miedzy-innymi-m.jpg'
     )
   ];
 
