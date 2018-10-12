6619c264388778b679d5891e14aea99975cb16c5
MKA1500 <maciejkabsch@gmail.com>
2018-10-12 08:45:55 +0200

    more services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,35 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe(
       'Kanapka z jajkiem i awokado',
       'Kanapka z awokado i jajkiem to propozycja na śniadanie albo pomysł na pyszną przekąskę, którą przygotujemy w kilka chwil. Zapraszam Was także na grzanki z awokado i pomidorkami oraz pastę kanapkową z awokado.',
-      'http://bi.im-g.pl/im/8f/ae/16/z23783567IF,Pelnowartosciowe-zrodla-bialka--to-miedzy-innymi-m.jpg'
+      'http://bi.im-g.pl/im/8f/ae/16/z23783567IF,Pelnowartosciowe-zrodla-bialka--to-miedzy-innymi-m.jpg',
+      [
+        new Ingredient('jajka', 2),
+        new Ingredient('awokado', 2),
+        new Ingredient('chleb', 1),
+        new Ingredient('majonez', 1),
+        new Ingredient('cebula', 1),
+        new Ingredient('rukola', 1)
+      ]
     ),
     new Recipe(
-      'Testing Recipe',
-      'This is simply a test',
-      'http://bi.im-g.pl/im/b9/ab/16/z23770809IF,Mojito-to-doskonaly-sposob-na-orzezwienie-w-cieple.jpg'
+      'Mohito',
+      'Drink mojito to jeden z najpopularniejszych koktajli na świecie. Jak zrobić mojito? Zobacz sprawdzony przepis na mojito prosto ze słonecznej Kuby!',
+      'http://bi.im-g.pl/im/b9/ab/16/z23770809IF,Mojito-to-doskonaly-sposob-na-orzezwienie-w-cieple.jpg',
+      [
+        new Ingredient('limonka', 1),
+        new Ingredient('cukier', 2),
+        new Ingredient('mięta', 3),
+        new Ingredient('rum', 1),
+        new Ingredient('lód', 1)
+      ]
     )
   ];
 
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,22 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
 export class ShoppingListService {
-  
+  ingredientsChange = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Herbes de Provence', 1),
+    new Ingredient('Onion', 2)
+  ];
+
+  getIngredients() {
+    // return copy:
+    return this.ingredients.slice();
+    // but in this case we will need to inform our component that some new data
+    // is available => ingredientsChange
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChange.emit(this.ingredients.slice());
+  }
 }
