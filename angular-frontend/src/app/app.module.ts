0e2b14cc36b4afc55acf3a0a2f594bc0f835ed43
MKA1500 <maciejkabsch@gmail.com>
2019-03-17 11:32:06 +0100

    lasy loading of recipes component

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
@@ -23,7 +22,6 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     HttpModule,
-    RecipesModule,
     ShoppingListModule,
     AuthModule,
     SharedModule,
